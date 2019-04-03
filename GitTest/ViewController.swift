17efdd72ce93363c554ed8efe5c5b7f9694ec4f8
Antony Tartakovskiy <atartakovskiy2022@steam-6.local>
2019-04-03 11:34:30 -0700

    hi

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
